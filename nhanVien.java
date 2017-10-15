3800a055a317d2cc504acba6a46cb6adb499776c
Duyen97 <32337071+Duyen97@users.noreply.github.com>
2017-10-15 19:25:29 +0700

    Add files via upload

---
+++ nhanVien.java
@@ -0,0 +1,35 @@
+package nhanvien;
+
+public class nhanVien {
+  String tenNhanVien;
+  int tuoiNhanVien;
+  String diaChiNhanVien;
+  String emailNhanVien;
+
+
+  public String getTenNhanVien() {
+    return tenNhanVien;
+  }
+  public void setTenNhanVien(String tenNhanVien) {
+    this.tenNhanVien =  tenNhanVien;
+  }
+  public int getTuoiNhanVien() {
+    return tuoiNhanVien;
+  }
+  public void setTuoiNhanVien() {
+    this.tuoiNhanVien = tuoiNhanVien;
+  }
+  public String getDiaChiNhanVien() {
+    return diaChiNhanVien;
+  }
+  public void setDiaChiNhanVien(String diaChiNhanVien) {
+    this.diaChiNhanVien = diaChiNhanVien;
+  }
+  public String getEmailNhanVien() {
+    return emailNhanVien;
+  }
+  public void setEmailNhanVien(String emailNhanVien) {
+    this.emailNhanVien = emailNhanVien;
+  }
+  
+}
